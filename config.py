53cf7de1ccc4ce54dc37ddecc3c759a21724efd7
marzz11 <126739101+marzz11@users.noreply.github.com>
2024-08-07 22:56:01 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 BOT_TOKEN = 'MTI3MDI1NDU0NDcxMzk0NTE0Mg.GTr2Xf.zown2TjYGWjv95_n3sspV2qBS6csDT0w91DDj0'
 ADMIN_USERNAME = '_.marzz._'
-NOTIFICATION_CHANNEL_ID = 1260500377455951944  # Replace with your notification channel ID
+NOTIFICATION_CHANNEL_ID =
 BACKUP_FILE_PATH = './data/backup.json'
 LOG_FILE_PATH = './data/passes_info.txt'
